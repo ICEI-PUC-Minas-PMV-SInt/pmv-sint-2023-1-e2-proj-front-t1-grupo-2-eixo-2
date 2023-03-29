39fdfbefc86ba3ff12d919caf7043ad67e0da789
Michele Freitas <michele@pop-os.localdomain>
2023-03-29 19:33:27 -0300

    Instalação de MUI e pacotes

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/system";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      100: "red",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <Home />
-    <Footer />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Home />
+      <Footer />
+    </ThemeProvider>
   </React.StrictMode>
 );
