c70165cab6b076ebb69a4352e8fb95754bfca0a1
Michele Freitas <michele@pop-os.localdomain>
2023-03-30 23:01:45 -0300

    Finalização da estrutura de pastas.

--- webapp/src/Router.jsx
+++ webapp/src/Router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Artigo } from "./pages/Artigo";
-import { Coleta } from "./pages/Coleta";
-import { Eventos } from "./pages/Eventos";
-import { Instrucoes } from "./pages/Instrucoes";
-import { Noticia } from "./pages/Noticia";
-import { Noticias } from "./pages/Noticias";
-import { Projeto } from "./pages/Projeto";
+import { Home } from "./pages/Home/Home";
+import { Artigo } from "./pages/Artigo/Artigo";
+import { Coleta } from "./pages/Coleta/Coleta";
+import { Eventos } from "./pages/Eventos/Eventos";
+import { Instrucoes } from "./pages/Instrucoes/Instrucoes";
+import { Noticia } from "./pages/Noticia/Noticia";
+import { Noticias } from "./pages/Noticias/Noticias";
+import { Projeto } from "./pages/Projeto/Projeto";
 
 export function Router() {
   return (
