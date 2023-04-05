6e4a79183b34e7ca0990fc8f308b4c12e7b3a6a0
Michele Freitas <michele@pop-os.localdomain>
2023-04-05 12:23:12 -0300

    Configurações gerais de estilo e estrutura inicial da página Home

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,11 +1,15 @@
-import { createTheme, ThemeProvider } from "@mui/material";
-import { green, lightBlue } from "@mui/material/colors";
+import { Container, createTheme, Paper, ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { green, lightBlue, grey } from "@mui/material/colors";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 
 const theme = createTheme({
   palette: {
+    background: {
+      default: grey[50],
+    },
     primary: {
       main: green[500],
     },
@@ -21,9 +25,14 @@ const theme = createTheme({
 export function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Paper>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </Paper>
+      </Container>
     </ThemeProvider>
   );
 }
