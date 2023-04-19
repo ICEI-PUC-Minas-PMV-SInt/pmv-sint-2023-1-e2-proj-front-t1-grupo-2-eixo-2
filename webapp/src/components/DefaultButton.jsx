6e053a7f8b0e19c30ba0d1286f052ade3d436b9c
Andrey Giovanni <andreygiovannircc@gmail.com>
2023-04-19 12:51:47 -0300

    Criado botão padrão para as duas cores

---
+++ webapp/src/components/DefaultButton.jsx
@@ -0,0 +1,36 @@
+import { Button, withStyles } from "@mui/material"
+import { green, lightBlue } from "@mui/material/colors";
+import PropTypes from 'prop-types'
+
+const styles = () => ({
+    default:{
+        borderRadius: 15,
+        textTransform: 'none'
+    },
+    primary:{
+        '&:hover':{
+            backgroundColor: green[900],
+            color: "#FFFFFF",
+        }
+    },
+    secondary:{
+        '&:hover':{
+            backgroundColor: lightBlue[800],
+            color:"#FFFFFF",
+        }
+    }
+})
+
+const DefaultButton = ({color, children, classes}) =>{
+    return(
+    <Button  variant="contained"color={text} classes={{root: classes.default, containedPrimary: classes.primary, containedSecondary: classes.secondary}} disableElevation>
+        {children}
+    </Button>
+    );
+};
+
+DefaultButton.PropTypes = {
+    color: PropTypes.string
+}
+
+export default withStyles(styles)(DefaultButton);
