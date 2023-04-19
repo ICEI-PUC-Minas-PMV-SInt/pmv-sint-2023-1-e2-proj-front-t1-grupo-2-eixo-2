95df3340652f9739ddd4a4bfbd54d5b522fee6d1
Michele Freitas <michele@pop-os.localdomain>
2023-04-19 18:35:32 -0300

    Correção de crash da aplicação

--- webapp/src/pages/Eventos/Eventos.jsx
+++ webapp/src/pages/Eventos/Eventos.jsx
@@ -1,5 +1,4 @@
-import { Box, Typography } from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Box, Typography, Grid } from "@mui/material";
 import { EventCard } from "./components/EventCard";
 import IMG_RIO_ALMAS from "../../assets/rio-das-almas.png";
 import IMG_CACHOEIRA from "../../assets/cachoeira-andorinhas.png";
@@ -11,11 +10,11 @@ function Eventos() {
     <Box p={8}>
       <Typography variant="h4">Calendário de Eventos</Typography>
       {/* Eventos Futuros */}
-      <Grid2 container mt={5}>
+      <Grid container mt={5}>
         <Typography variant="h5">Eventos Futuros</Typography>
-        <Grid2 mt={1.5} container spacing={4}>
+        <Grid mt={1.5} container spacing={4}>
           {eventList.map((event) => (
-            <Grid2 xs={12} sm={6} md={3}>
+            <Grid item xs={12} sm={6} md={3}>
               <EventCard
                 img={IMG_RIO_ALMAS}
                 title={event.title}
@@ -23,17 +22,17 @@ function Eventos() {
                 eventTime={event.eventTime}
                 moreInfo={event.moreInfo}
               ></EventCard>
-            </Grid2>
+            </Grid>
           ))}
-        </Grid2>
-      </Grid2>
+        </Grid>
+      </Grid>
       {/* Fim Eventos Futuros */}
 
       {/* Eventos Passados */}
-      <Grid2 container mt={5}>
+      <Grid container mt={5}>
         <Typography variant="h5">Eventos Passados</Typography>
-        <Grid2 mt={1.5} container spacing={4}>
-          <Grid2 xs={12} sm={6} md={3}>
+        <Grid mt={1.5} container spacing={4}>
+          <Grid item xs={12} sm={6} md={3}>
             <EventCard
               img={IMG_CACHOEIRA}
               title={"Mutirão de Limpeza Cachoeira Andorinhas"}
@@ -43,8 +42,8 @@ function Eventos() {
                 "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
               }
             ></EventCard>
-          </Grid2>
-          <Grid2 xs={12} sm={6} md={3}>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
             <EventCard
               img={IMG_RIO_ALMAS}
               title={"Mutirão de Limpeza do Rio das Almas"}
@@ -54,8 +53,8 @@ function Eventos() {
                 "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
               }
             ></EventCard>
-          </Grid2>
-          <Grid2 xs={12} sm={6} md={3}>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
             <EventCard
               img={IMG_CACHOEIRA}
               title={"Mutirão de Limpeza Cachoeira Andorinhas"}
@@ -65,8 +64,8 @@ function Eventos() {
                 "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
               }
             ></EventCard>
-          </Grid2>
-          <Grid2 xs={12} sm={6} md={3}>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
             <EventCard
               img={IMG_RIO_ALMAS}
               title={"Mutirão de Limpeza do Rio das Almas"}
@@ -76,9 +75,9 @@ function Eventos() {
                 "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
               }
             ></EventCard>
-          </Grid2>
-        </Grid2>
-      </Grid2>
+          </Grid>
+        </Grid>
+      </Grid>
       {/* Fim Eventos Passados */}
     </Box>
   );
