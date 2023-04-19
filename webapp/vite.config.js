b17e6f9bc9072241b169f06cc77ed9566e29315b
Nicolas Wulk <nicolas.wulk@cnj.jus.br>
2023-04-19 13:38:35 -0300

    Removendo workflow do Github Pages e alterando config do Vite

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pmv-sint-2023-1-e2-proj-front-t1-grupo-2-eixo-2/",
 });
