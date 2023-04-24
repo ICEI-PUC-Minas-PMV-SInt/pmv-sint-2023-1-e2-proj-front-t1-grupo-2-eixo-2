43f3e1c06afef69ba6573bcf084cc82024e437ed
Nicolas Wulk <nicolas.wulk@cnj.jus.br>
2023-04-24 11:48:01 -0300

    Ajustes no estilo dos cards de destaque da Home

--- webapp/src/pages/Home/Home.jsx
+++ webapp/src/pages/Home/Home.jsx
@@ -1,11 +1,6 @@
 import { Card, CardHeader, Typography, Grid } from "@mui/material";
 import { Header } from "../../components/Header";
-import {
-  CardHomeEventos,
-  CardHomeInfo,
-  CardHomeReciclagem,
-  CardHomeRotas,
-} from "../../components/CardHome";
+import { DestaquesHome } from "../../components/CardHome";
 
 function Home() {
   return (
@@ -16,38 +11,8 @@ function Home() {
       <Grid item height={400} xs={12}>
         <Typography>Banner Destaque</Typography>
       </Grid>
-      <Grid
-        container
-        spacing={2}
-        justifyContent="space-evenly"
-        alignItems="stretch"
-        sx={{
-          justifyContent: "center",
-          display: "flex",
-          alignItems: "center",
-          textAlign: "center",
-          boxShadow: 0,
-          border: 0,
-          margin: 0,
-          padding: "1 rem 1.5rem",
-          borderRadius: 8,
-          boxShadow: "0 0 0 0",
-        }}
-      >
-        <Grid item sm={6} md={3}>
-          <CardHomeEventos />
-        </Grid>
-        <Grid item sm={6} md={3}>
-          <CardHomeReciclagem />
-        </Grid>
-        <Grid item sm={6} md={3}>
-          <CardHomeInfo />
-        </Grid>
-        <Grid item sm={6} md={3}>
-          <CardHomeRotas />
-        </Grid>
-      </Grid>
-    <Grid item height={200} xs={12}>
+      <DestaquesHome />
+      <Grid item height={200} xs={12}>
         <Typography>Parceiros</Typography>
       </Grid>
       <Grid item height={500} xs={12}>
