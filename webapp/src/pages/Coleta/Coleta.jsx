8aac24b108e2bcee9662b5a3fba48a86420f1830
Nicolas Wulk <nicolas.wulk@cnj.jus.br>
2023-04-24 22:18:06 -0300

    Criando componentes e página para Coleta

--- webapp/src/pages/Coleta/Coleta.jsx
+++ webapp/src/pages/Coleta/Coleta.jsx
@@ -1,7 +1,60 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { MapaColeta } from "../../components/MapaColeta";
+import {
+  DiaEntregaCard,
+  DiaSemEntregaCard,
+} from "../../components/CardDiasColeta";
 
+//função para criar a página de coleta, incluindo o mapa e os dias da semana
 function Coleta() {
-  return <Typography variant="h2">COLETA</Typography>;
+  return (
+    <Grid container justifyContent="center" alignItems="center" padding={2}>
+      <Header />
+      <Grid item xs={12}>
+        <Typography variant="h3" xs={12} padding={2}>
+          Coleta Seletiva em Pirenópolis / Goiás
+        </Typography>
+      </Grid>
+
+      <MapaColeta />
+
+      <Grid item xs={12} p={2}>
+        <Typography variant="h4">Horários da Coleta Seletiva</Typography>
+      </Grid>
+      <Grid
+        container
+        spacing={0}
+        marginBottom={4}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="baseline"
+        sx={{ backgroundColor: "inherit" }}
+      >
+        <DiaEntregaCard
+          dia={"Segunda-feira"}
+          horarioInicio={"7"}
+          horarioFim={"12"}
+        />
+        <DiaSemEntregaCard dia={"Terça-feira"} />
+        <DiaEntregaCard
+          dia={"Quarta-feira"}
+          horarioInicio={"7"}
+          horarioFim={"12"}
+        />
+        <DiaSemEntregaCard dia={"Quinta-feira"} />
+        <DiaEntregaCard
+          dia={"Sexta-feira"}
+          horarioInicio={"7"}
+          horarioFim={"12"}
+        />
+        <DiaSemEntregaCard dia={"Sábado"} />
+        <DiaSemEntregaCard dia={"Domingo"} />
+      </Grid>
+      <Footer />
+    </Grid>
+  );
 }
 
 export { Coleta };
