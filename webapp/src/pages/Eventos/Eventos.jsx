788767307d81bf170b13db9bec760ca1b453868f
Michele Freitas <michele@pop-os.localdomain>
2023-04-27 18:48:50 -0300

    Conclusão Página de Eventos

--- webapp/src/pages/Eventos/Eventos.jsx
+++ webapp/src/pages/Eventos/Eventos.jsx
@@ -1,85 +1,60 @@
 import { Box, Typography, Grid } from "@mui/material";
 import { EventCard } from "./components/EventCard";
-import IMG_RIO_ALMAS from "../../assets/rio-das-almas.png";
-import IMG_CACHOEIRA from "../../assets/cachoeira-andorinhas.png";
-import IMG_PIRINEUS from "../../assets/mutirao-limpeza-parque.png";
-import { eventList } from "./mockDb";
+import { eventList, pastEventsList } from "./mockDb";
+
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
 
 function Eventos() {
   return (
-    <Box p={8}>
-      <Typography variant="h4">Calendário de Eventos</Typography>
-      {/* Eventos Futuros */}
-      <Grid container mt={5}>
-        <Typography variant="h5">Eventos Futuros</Typography>
-        <Grid mt={1.5} container spacing={4}>
-          {eventList.map((event) => (
-            <Grid item xs={12} sm={6} md={3}>
-              <EventCard
-                img={IMG_RIO_ALMAS}
-                title={event.title}
-                eventDate={event.eventDate}
-                eventTime={event.eventTime}
-                moreInfo={event.moreInfo}
-              ></EventCard>
-            </Grid>
-          ))}
-        </Grid>
+    <Grid container justifyContent="center" alignItems="center">
+      <Grid item height={108} xs={12} bgcolor={"white"}>
+        <Header />
       </Grid>
-      {/* Fim Eventos Futuros */}
+      <Box p={8}>
+        <Typography variant="h4">Calendário de Eventos</Typography>
+        {/* Eventos Futuros */}
+        <Grid container mt={5}>
+          <Typography variant="h5">Eventos Futuros</Typography>
+          <Grid mt={1.5} container spacing={4}>
+            {eventList.map((event) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <EventCard
+                  img={event.img}
+                  title={event.title}
+                  eventDate={event.eventDate}
+                  eventTime={event.eventTime}
+                  moreInfo={event.moreInfo}
+                ></EventCard>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        {/* Fim Eventos Futuros */}
 
-      {/* Eventos Passados */}
-      <Grid container mt={5}>
-        <Typography variant="h5">Eventos Passados</Typography>
-        <Grid mt={1.5} container spacing={4}>
-          <Grid item xs={12} sm={6} md={3}>
-            <EventCard
-              img={IMG_CACHOEIRA}
-              title={"Mutirão de Limpeza Cachoeira Andorinhas"}
-              eventDate={"17/02/2023"}
-              eventTime={"08:00hs"}
-              moreInfo={
-                "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
-              }
-            ></EventCard>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <EventCard
-              img={IMG_RIO_ALMAS}
-              title={"Mutirão de Limpeza do Rio das Almas"}
-              eventDate={"03/08/2022"}
-              eventTime={"09:00hs"}
-              moreInfo={
-                "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
-              }
-            ></EventCard>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <EventCard
-              img={IMG_CACHOEIRA}
-              title={"Mutirão de Limpeza Cachoeira Andorinhas"}
-              eventDate={"19/07/2022"}
-              eventTime={"09:00hs"}
-              moreInfo={
-                "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
-              }
-            ></EventCard>
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <EventCard
-              img={IMG_RIO_ALMAS}
-              title={"Mutirão de Limpeza do Rio das Almas"}
-              eventDate={"03/05/2022"}
-              eventTime={"09:00hs"}
-              moreInfo={
-                "Informações adicionais sobre o Evento, local de encontro, duração, o que levar, etc."
-              }
-            ></EventCard>
+        {/* Eventos Passados */}
+        <Grid container mt={5}>
+          <Typography variant="h5">Eventos Passados</Typography>
+          <Grid mt={1.5} container spacing={4}>
+            {pastEventsList.map((event) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <EventCard
+                  img={event.img}
+                  title={event.title}
+                  eventDate={event.eventDate}
+                  eventTime={event.eventTime}
+                  moreInfo={event.moreInfo}
+                ></EventCard>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
+        {/* Fim Eventos Passados */}
+      </Box>
+      <Grid item height={500} xs={12}>
+        <Footer />
       </Grid>
-      {/* Fim Eventos Passados */}
-    </Box>
+    </Grid>
   );
 }
 export { Eventos };
--- webapp/src/pages/Eventos/mockDb.js
+++ webapp/src/pages/Eventos/mockDb.js
@@ -1,44 +1,60 @@
+import IMG_RIO_ALMAS from "../../assets/rio-das-almas.png";
+import IMG_CACHOEIRA from "../../assets/cachoeira-andorinhas.png";
+import IMG_PIRINEUS from "../../assets/mutirao-limpeza-parque.png";
+import IMG_RIO_ALMAS2 from "../../assets/ponte-rio-das-almas.png";
+import IMG_RIO_CACHOEIRA2 from "../../assets/cachoeira-andorinhas-alto.png";
+import IMG_PIRINEUS2 from "../../assets/parque-estadual-pirineus.png";
+
 export const eventList = [
   {
-    img: "/assets/mutirao-limpeza-parque.png",
-    title: "Mutirão...",
-    eventDate: "01/01/2023",
-    eventTime: "10:00h",
-    moreInfo: "Mais informações do evento",
+    img: IMG_RIO_ALMAS,
+    title: "Mutirão de Limpeza no Rio das Almas",
+    eventDate: "01/05/2023",
+    eventTime: "09:00h",
+    moreInfo:
+      "Vamos realizar mais um mutirão de limpeza nas margens do Rio das Almas, o ponto de encontro será na praça da Ponte Nova, em frente aos Bombeiros. Convide os amigos e venha participar dessa missão em prol do meio ambiente e do planeta. Venha de máscara e luva.",
   },
   {
-    img: "/assets/mutirao-limpeza-parque.png",
-    title: "Mutirão 2...",
-    eventDate: "01/01/2023",
-    eventTime: "10:00h",
-    moreInfo: "Mais informações do evento",
+    img: IMG_PIRINEUS,
+    title: "Mutirão de Limpeza na entrada para os Pirineus",
+    eventDate: "05/06/2023",
+    eventTime: "08:00h",
+    moreInfo:
+      "Bora pra mais um mutirão de limpeza? Dessa vez vamos recolher um tanto de lixo eletrônico que foi abandonado próximo ao Rio das Almas, no final do asfalto indo pro morro dos Pireneus. O ponto de encontro é na praça central as 8h.",
   },
   {
-    img: "/assets/mutirao-limpeza-parque.png",
-    title: "Mutirão 3...",
-    eventDate: "01/01/2023",
-    eventTime: "10:00h",
-    moreInfo: "Mais informações do evento",
+    img: IMG_CACHOEIRA,
+    title: "Mutirão d Limpeza na Cachoeira Andorinhas",
+    eventDate: "03/07/2023",
+    eventTime: "08:00h",
+    moreInfo:
+      "Bora ajudar a limpar nosso paraíso? Neste sábado tem mutirão de limpeza na região da cachoeira das Andorinhas! O ponto de encontro é na praça central, às 08h00, onde vamos organizar as caronas nos carros disponíveis.",
+  },
+];
+
+export const pastEventsList = [
+  {
+    img: IMG_RIO_ALMAS2,
+    title: "Mutirão de Limpeza no Rio das Almas",
+    eventDate: "01/05/2022",
+    eventTime: "09:00h",
+    moreInfo:
+      "Vamos realizar mais um mutirão de limpeza nas margens do Rio das Almas, o ponto de encontro será na praça da Ponte Nova, em frente aos Bombeiros. Convide os amigos e venha participar dessa missão em prol do meio ambiente e do planeta. Venha de máscara e luva.",
   },
   {
-    img: "/assets/mutirao-limpeza-parque.png",
-    title: "Mutirão...",
-    eventDate: "01/01/2023",
-    eventTime: "10:00h",
-    moreInfo: "Mais informações do evento",
+    img: IMG_RIO_CACHOEIRA2,
+    title: "Mutirão d Limpeza na Cachoeira Andorinhas",
+    eventDate: "05/06/2022",
+    eventTime: "08:00h",
+    moreInfo:
+      "Bora pra mais um mutirão de limpeza? Dessa vez vamos recolher um tanto de lixo eletrônico que foi abandonado próximo ao Rio das Almas, no final do asfalto indo pro morro dos Pireneus. O ponto de encontro é na praça central as 8h.",
   },
   {
-    img: "/assets/mutirao-limpeza-parque.png",
-    title: "Mutirão...",
-    eventDate: "01/01/2023",
-    eventTime: "10:00h",
-    moreInfo: "Mais informações do evento",
-  },
-  {
-    img: "/assets/mutirao-limpeza-parque.png",
-    title: "Mutirão...",
-    eventDate: "01/01/2023",
-    eventTime: "10:00h",
-    moreInfo: "Mais informações do evento",
+    img: IMG_PIRINEUS2,
+    title: "Mutirão de Limpeza na entrada para os Pirineus",
+    eventDate: "03/07/2022",
+    eventTime: "08:00h",
+    moreInfo:
+      "Bora ajudar a limpar nosso paraíso? Neste sábado tem mutirão de limpeza na região da cachoeira das Andorinhas! O ponto de encontro é na praça central, às 08h00, onde vamos organizar as caronas nos carros disponíveis.",
   },
 ];
