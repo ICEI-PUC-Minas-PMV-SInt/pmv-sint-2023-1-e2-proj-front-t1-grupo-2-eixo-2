15c680ddc8b715f7cb9d7c5057237e3e0b1e32e7
Matheus-9 <matheusexu@gmail.com>
2023-05-16 14:31:07 -0300

    Tornando os cards clicavel

--- webapp/src/pages/Noticias/Noticias.jsx
+++ webapp/src/pages/Noticias/Noticias.jsx
@@ -15,42 +15,45 @@ import escola from '../../assets/escola.jpg';
 
 
 
-
-
 function Noticias() {
   // Array de notícias
   const noticias = [
     {
       titulo: 'Comunidade se une em prol da reciclagem e transforma resíduos em arte',
-      imagem:  imagen01,
+      imagem: imagen01,
       conteudo: 'Conteúdo da notícia principal',
+      link: 'link-da-noticia-1',
     },
     {
       titulo: 'Cidade implementa programa de reciclagem e reduzirá emissão de resíduos',
       imagem: reduzida,
       conteudo: 'A cidade XYZ anunciou o lançamento de um programa abrangente de reciclagem...',
+      link: 'link-da-noticia-2',
     },
     {
       titulo: 'Iniciativa de reciclagem transforma resíduos em materiais de construção',
       imagem: iniciativa,
       conteudo: 'Uma nova iniciativa de reciclagem está transformando resíduos em recursos valiosos...',
+      link: 'link-da-noticia-3',
     },
     {
       titulo: 'Empresa lança programa de coleta de eletrônicos para reciclagem ...',
       imagem: empresa,
       conteudo: 'Uma empresa líder em tecnologia anunciou o lançamento de um programa de coleta de eletrônicos...',
+      link: 'link-da-noticia-4',
     },
     {
       titulo: 'Estudantes promovem campanha de conscientização sobre reciclagem nas esco...',
       imagem: escola,
       conteudo: 'Um grupo de estudantes entusiasmados está fazendo a diferença na comunidade por meio de uma campanha...',
+      link: 'link-da-noticia-5',
     },
   ];
 
   return (
     <div>
       <Card>
-      <Header />
+        <Header />
 
         <CardMedia
           component="img"
@@ -71,12 +74,14 @@ function Noticias() {
         {noticias.slice(1).map((noticia, index) => (
           <Grid item xs={12} sm={6} md={3} key={index}>
             <Card>
-              <CardMedia
-                component="img"
-                height="200"
-                image={noticia.imagem}
-                alt={`Imagem da notícia ${index + 1}`}
-              />
+              <a href={noticia.link} target="_blank" rel="noopener noreferrer">
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={noticia.imagem}
+                  alt={`Imagem da notícia ${index + 1}`}
+                />
+              </a>
               <CardContent>
                 <Typography variant="h5" component="h2">
                   {noticia.titulo}
@@ -90,7 +95,6 @@ function Noticias() {
         ))}
       </Grid>
       <Footer />
-
     </div>
   );
 }
